61cb0a6ed2bbe7684458ae4fe9e2fad31ec06e60
Daniel Baker <dbaker@vistaprint.com>
2016-07-11 13:45:16 +0200

    First test on the NewStreamHelper

---
+++ FileHelpers.Tests/Tests/Helpers/NewStreamHelperShould.cs
@@ -0,0 +1,31 @@
+﻿
+using System.IO;
+using System.Text;
+using NFluent;
+using NUnit.Framework;
+
+namespace FileHelpers.Tests.Helpers
+{
+    [TestFixture]
+    public class NewStreamHelperShould
+    {
+        [Test]
+        public void not_correct_end()
+        {
+            NewStreamHelper newStreamHelper = new NewStreamHelper();
+
+            Check.That(newStreamHelper
+                        .CreateFileAppender(new MemoryStream(), Encoding.ASCII, false, true, 8)
+                    .Encoding)
+                .Equals(Encoding.ASCII);
+        }
+    }
+
+    public class NewStreamHelper
+    {
+        public StreamWriter CreateFileAppender(MemoryStream memoryStream, Encoding encode, bool correctEnd, bool disposeStream, int bufferSize)
+        {
+            return new StreamWriter(memoryStream, encode, bufferSize);
+        }
+    }
+}
